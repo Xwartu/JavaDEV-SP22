b507df3a7f09297da052c2f8a3678aade2175f19
Josh Tonner <jtonner@iu.edu>
2022-01-12 12:24:28 -0500

    Class

--- .idea/HelloWorld.java
+++ .idea/HelloWorld.java
@@ -3,7 +3,8 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-
-        System.out.ln("Hello World!");
+        int num = 10;
+        System.out.println(num);
+        System.out.println("Hello World!");
     }
 }
