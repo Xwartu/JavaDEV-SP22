ca1642f43c39db6da27288119a881bd7705aa341
Josh Tonner <jtonner@iu.edu>
2022-01-13 09:44:18 -0500

    .idea Structure

--- Lab1/.idea/Lab1.java
+++ Lab1/.idea/Lab1.java
@@ -3,6 +3,8 @@
  */
 public class Lab1 {
     public static void main(String[] args) {
-        System.out.print("*")
+        int num = 10;
+        System.out.println(num);
+        System.out.println("*")
     }
 }
