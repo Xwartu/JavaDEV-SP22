b8d46132ad6ea521834d5b208ecccc376a316091
Josh Tonner <jtonner@iu.edu>
2022-01-19 21:49:57 -0500

    Maybe working

--- Lab1/src/Lab1.java
+++ Lab1/src/Lab1.java
@@ -1,2 +1,53 @@
 public class Lab1 {
+    public static void main(String[] args)
+    {
+        System.out.print("Printing pattern (a)\n\n\n");
+        System.out.print("*");
+        System.out.print("**");
+        System.out.print("***");
+        System.out.print("****");
+        System.out.print("*****");
+        System.out.print("******");
+        System.out.print("*******");
+        System.out.print("********");
+        System.out.print("*********");
+        System.out.print("**********");
+
+        System.out.print("Printing pattern (b)\n\n\n");
+        System.out.print("**********");
+        System.out.print("*********");
+        System.out.print("********");
+        System.out.print("*******");
+        System.out.print("******");
+        System.out.print("*****");
+        System.out.print("****");
+        System.out.print("***");
+        System.out.print("**");
+        System.out.print("*");
+
+        System.out.print("Printing pattern (c)\n\n\n");
+        System.out.print("**********");
+        System.out.print(" *********");
+        System.out.print("  ********");
+        System.out.print("   *******");
+        System.out.print("    ******");
+        System.out.print("     *****");
+        System.out.print("      ****");
+        System.out.print("       ***");
+        System.out.print("        **");
+        System.out.print("         *");
+
+
+        System.out.print("Printing pattern (d)\n\n\n");
+        System.out.print("         *");
+        System.out.print("        **");
+        System.out.print("       ***");
+        System.out.print("      ****");
+        System.out.print("     *****");
+        System.out.print("    ******");
+        System.out.print("   *******");
+        System.out.print("  ********");
+        System.out.print(" *********");
+        System.out.print("**********");
+    }
 }
